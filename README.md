ecf9dea3059de903f8ea4c6771e577090fb47d6c
Andrii Sydorenko <a.sydorenko@fondy.eu>
2024-05-24 11:02:17 +0300

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+Module for Magento 1.9
+=====
+#EN
+
+Installation
+----
+Copy all files to `{site root directory}/app/`
+
+>1. Login to admin interface
+
+>2. Navigate to menu "System" -> "Configuration" 
+
+>3. Open tab "Payment methods"
+
+>4. Choose Alliance bank or Alliance bank on Page
+
+>5. Enable module
+
+>N.B.! Available statuses after order completion are Processing, On Hold 
+
+#RU
+
+Установка
+----
+Содержимое скопировать в папку `{корень сайта}/app/`
+
+>1. Зайти в панель администратора
+
+>2. Перейти в раздел "Система" -> "Конфигурация" ( "System" -> "Configuration" )
+
+>3. Открыть вкладку "Платежные методы" ( "Payment methods" )
+
+>4. Выбрать Alliance bank или Alliance bank on Page
+
+>5. Включить модуль и ввести все настройки 
+
+>!!!!Доступные статусы после завершения заказа: Processing, On Hold 
+
+
+Callback URL : `http://yoursite/Fondy/response`
+-----
+
+Example of "Alliance bank on Page" settings:
+
+[1]: https://raw.githubusercontent.com/cloudipsp/magento/master/magentof.png
+![Скриншот][1]
+
+Example of "Alliance bank" settings:
+
+[2]: https://raw.githubusercontent.com/cloudipsp/magento/master/magentor.png
+![Скриншот][2]
---
+++ code/local/Fondy/FondyOnPage/Model/FondyOnPage.php
@@ -0,0 +1,106 @@
+<?php
+
+class Fondy_FondyOnPage_Model_FondyOnPage extends Mage_Payment_Model_Method_Abstract
+{
+
+    protected $_code = 'FondyOnPage';
+    protected $_formBlockType = 'FondyOnPage/form';
+
+    public function getCheckout()
+    {
+        return Mage::getSingleton('checkout/session');
+    }
+
+    public function getOrderPlaceRedirectUrl()
+    {
+        return Mage::getUrl('FondyOnPage/checkout', array('_secure' => true));
+    }
+
+    public function getQuote()
+    {
+        $orderIncrementId = $this->getCheckout()->getLastRealOrderId();
+        $order = Mage::getModel('sales/order')->loadByIncrementId($orderIncrementId);
+        return $order;
+    }
+
+    public function getFormFields()
+    {
+        include_once "Fondy.cls.php";
+        $order_id = $this->getCheckout()->getLastRealOrderId();
+        $order = Mage::getModel('sales/order')->loadByIncrementId($order_id);
+        $amount = round($order->getGrandTotal() * 100);
+
+        $customer = Mage::getSingleton('customer/session')->getCustomer();
+        $quote = Mage::getSingleton('checkout/session')->getQuote();
+
+        $email = $customer->getEmail();
+        $email = isset($email) ? $email : $quote->getBillingAddress()->getEmail();
+        $email = isset($email) ? $email : $order->getCustomerEmail();
+
+        $callback = Mage::getUrl('FondyOnPage/response', array('_secure' => true, '_query' => 'callback=1'));
+        $response_url = $this->getConfigData('back_ref');
+
+        $fields = array(
+            'order_id' => $order_id . FondyForm::ORDER_SEPARATOR . time(),
+            'merchant_id' => $this->getConfigData('merchant'),
+            'order_desc' => Mage::helper('sales')->__('Order #') . $order_id,
+            'amount' => $amount,
+            'currency' => $this->getConfigData('currency'),
+            'server_callback_url' => $callback,
+            'response_url' => !empty($response_url) ? $response_url : Mage::getUrl('checkout/onepage/success', array('_secure' => true)),
+            'lang' => $this->getConfigData('language'),
+            'sender_email' => $email
+        );
+
+        $fields['signature'] = FondyForm::getSignature($fields, $this->getConfigData('secret_key'));
+        Mage::log('Request: ' . json_encode($fields), null, 'fondy.log', false);
+        $params = array();
+        try {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'https://payment.albpay.io/api/checkout/url/');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('request' => $fields)));
+            $result = json_decode(curl_exec($ch), TRUE);
+            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            if ($httpcode != 200) {
+                Mage::log('Curl code: ' . $httpcode, null, 'fondy.log', true);
+                $params = array(
+                    'error' => 1,
+                    'message' => 'Curl error http code not 200, code is: ' . $httpcode
+                );
+                return $params;
+            }
+            if ($result['response']['response_status'] == 'failure') {
+                Mage::log('Response: ' . $result['response']['error_message'], null, 'fondy.log', true);
+                $params = array(
+                    'error' => 1,
+                    'message' => $result['response']['error_message']
+                );
+            } else {
+                if (isset($result['response']['checkout_url'])) {
+                    Mage::log('Result: ' . json_encode($result), null, 'fondy.log', false);
+                    $params = array(
+                        'url' => $result['response']['checkout_url'],
+                        'styles' => $this->getConfigData('styles')
+                    );
+                } else {
+                    Mage::log('Result: ' . json_encode($result), null, 'fondy.log', true);
+                    $params = array(
+                        'error' => 1,
+                        'message' => json_encode($result)
+                    );
+                }
+            }
+
+        } catch (Exception $e) {
+            Mage::log('Error: ' . $e, null, 'fondy.log', true);
+            $params = array(
+                'error' => 1,
+                'message' => 'Undefined Error'
+            );
+        }
+        return $params;
+    }
+}
